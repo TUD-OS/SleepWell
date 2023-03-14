471188e96b8b13861e9ab7bd61aef8ff92e55739
Thomas Oberhauser <thomas.oberhauser@mailbox.tu-dresden.de>
2023-03-14 17:50:55 +0100

    Added NMI sending/handling

--- mwait_module/mwait.c
+++ mwait_module/mwait.c
@@ -2,9 +2,10 @@
 #include <linux/kernel.h>
 #include <asm/apic.h>
 #include <asm/msr.h>
+#include <asm/hw_irq.h>
+#include <asm/nmi.h>
 #include <linux/delay.h>
-
-#define MSR_PKG_ENERGY_STATUS 0x611
+#include <asm/msr-index.h>
 
 MODULE_LICENSE("GPL");
 
@@ -33,7 +34,7 @@ void test_function(void *info)
         {
             mdelay(1000);
             rdmsrl_safe(MSR_PKG_ENERGY_STATUS, &total_energy_consumed);
-            printk(KERN_INFO "CPU %i: Triggering iteration %i, trigger value: %i, total energy consumed: %i\n", this_cpu, i, i, total_energy_consumed);
+            printk(KERN_INFO "CPU %i: Triggering iteration %i, trigger value: %i, total energy consumed: %lli\n", this_cpu, i, i, total_energy_consumed);
             trigger = i;
         }
     }
@@ -42,7 +43,27 @@ void test_function(void *info)
 
 bool cond_function(int cpu, void *info)
 {
-    return cpu < 4;
+    return !cpu;
+}
+
+int nmi_handler(unsigned int val, struct pt_regs* regs) {
+    int this_cpu = smp_processor_id();
+
+    if(!this_cpu) {
+        printk(KERN_INFO "CPU %i: Sending NMIs", this_cpu);
+        apic->send_IPI_allbutself(NMI_VECTOR);
+	} else {
+        printk(KERN_INFO "CPU %i: Received NMI", this_cpu);
+    }
+
+    return NMI_HANDLED;
+}
+
+void measure_nop(void* info) {
+    int this_cpu = get_cpu();
+
+    printk(KERN_INFO "CPU %i: Sending NMIs", this_cpu);
+    apic->send_IPI_allbutself(NMI_VECTOR);
 }
 
 static int mwait_init(void)
@@ -64,13 +85,16 @@ static int mwait_init(void)
     unsigned cpus_online = num_online_cpus();
     printk(KERN_INFO "Online Cpus: %i\n", cpus_online);
 
-    on_each_cpu_cond(cond_function, test_function, NULL, 1);
+    register_nmi_handler(NMI_UNKNOWN, nmi_handler, 0, "nmi_handler");
+
+    on_each_cpu_cond(cond_function, measure_nop, NULL, 1);
 
     return 0;
 }
 
 static void mwait_exit(void)
 {
+    unregister_nmi_handler(NMI_UNKNOWN, "nmi_handler");
     printk(KERN_INFO "mwait exit\n");
 }
 
