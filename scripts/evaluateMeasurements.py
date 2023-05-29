7cdfaaff7f00812f360d77d0289ba7ca406ba76b
obibabobi <thomas-oberhauser@t-online.de>
2023-05-29 17:32:36 +0200

    continued evaluation script

--- scripts/evaluateMeasurements.py
+++ scripts/evaluateMeasurements.py
@@ -1,5 +1,18 @@
 import sys
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv(sys.argv[1])
-print(df)
+results_directory = sys.argv[1]
+
+def add_csv_as_column(df, name, pos):
+    new_df = pd.read_csv(results_directory+name, names=[name])
+    new_df /= 10000000
+    df.insert(pos, name, new_df[name])
+
+df = pd.DataFrame()
+add_csv_as_column(df, "C1", 0)
+add_csv_as_column(df, "C2", 1)
+plot = df.plot.box()
+plot.set_ylim(ymin=0)
+plt.show()
+plot.figure.savefig('test.png')
